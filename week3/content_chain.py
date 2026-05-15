97f6a948aedf7af665a4c5f3002c42dcd06cb7d0
yukey <yukey008@outlook.com>
2026-05-16 03:27:23 +0800

    refactor program version1

--- week3/content_chain.py
+++ week3/content_chain.py
@@ -1,6 +1,9 @@
 import os
+
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+import re
 # # 读取 api key
 def get_api_key():
     api_key = os.environ.get("DEEPSEEK_API_KEY")  # 请将个人的api key 可以加入到环境变量中
@@ -20,10 +23,7 @@ def get_model():
 def get_title_chain(llm):
     title = PromptTemplate(
         input_variables=["topic"],
-        template="请写一个关于{topic}的生成3个小红书笔记的标题。输出格式如下："
-                 "标题1： \n"
-                 "标题2： \n"
-                 "标题3: "
+        template="写一个关于{topic}的生成3个小红书笔记的标题。"
 
 
     )
@@ -33,18 +33,36 @@ def get_title_chain(llm):
 def get_content_chain(llm):
     content= PromptTemplate(
         input_variables=["title"],
-        template="从生成的3个标题中选择一个{title}，并向这个【标题】生成一篇包含emoji的正文草稿"
+        template="现在你作为小红书知名网红博主，从生成的3个标题中选择一个{title}，并向这个标题生成一篇包含emoji的正文草稿"
     )
     content_chain = content | llm
     return content_chain
+
+
+def extract_first_title(output):
+    lines = output.content.splitlines()
+    first_line = next((line for line in lines if line.strip()), "")
+    first_title = first_line.strip()
+    return {"title": first_title}
+
 def main():
     topic = input("请输入你想要的主题：")
     llm_model = get_model()
-    title_response = get_title_chain(llm_model).invoke({"topic": topic})
-    titles_text = title_response.content
-    first_title = titles_text.splitlines()[0]
-    content_response = get_content_chain(llm_model).invoke({"title": first_title})
-    print(f"{title_response.content}\n正文：{content_response.content}")
+    # title_response = get_title_chain(llm_model).invoke({"topic": topic})
+    # titles_text = title_response.content
+    # first_title = titles_text.splitlines()[0]
+    # content_response = get_content_chain(llm_model).invoke({"title": first_title})
+    full_chain = (
+            {"topic": RunnablePassthrough()}  # 输入原始 topic
+            | get_title_chain(llm_model)  # 生成3个标题
+            | RunnableLambda(extract_first_title)  # 提取第一个标题
+            | get_content_chain(llm_model)  # 生成正文
+    )
+
+    result = full_chain.invoke(topic)
+
+
+    print(result.content)
 
 if __name__ == "__main__":
         main()
